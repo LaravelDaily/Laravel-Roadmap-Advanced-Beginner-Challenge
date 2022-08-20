ea49ab023015c8107124546081fe6e8d3a30bbf6
Gardi <ahmadgardi0@gmail.com>
2022-08-20 16:07:20 +0300

    add dashboard ui

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
