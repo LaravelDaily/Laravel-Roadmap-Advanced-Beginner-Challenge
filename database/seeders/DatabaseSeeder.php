7ed98806f1f3ba86e8fc2f2d5671bdfc583353e4
Gardi <ahmadgardi0@gmail.com>
2022-08-23 15:05:06 +0300

    add Clients Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             UsersSeeder::class,
+            ClientsSeeder::class,
         ]);
     }
 }
