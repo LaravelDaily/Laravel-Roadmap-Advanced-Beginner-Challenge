4295807ccea17bb32cc4725ec2b23efb5a31e7ef
sodaler <misha_lector87@mail.ru>
2023-06-26 21:04:09 +0400

    Project factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\Project\ProjectStatusEnum;
+use App\Models\Client;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    protected $model = Project::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->words(5, true),
+            'description' => $this->faker->text(50),
+            'deadline' => $this->faker->date(),
+            'status' => $this->faker->randomElement(ProjectStatusEnum::cases()),
+            'user_id' => User::query()->inRandomOrder()->value('id'),
+            'client_id' => Client::query()->inRandomOrder()->value('id'),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Database\Factories\ClientFactory;
+use Database\Factories\ProjectFactory;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
 
@@ -17,5 +18,7 @@ class DatabaseSeeder extends Seeder
          UserFactory::new()->count(5)->create();
 
          ClientFactory::new()->count(10)->create();
+
+         ProjectFactory::new()->count(10)->create();
     }
 }
