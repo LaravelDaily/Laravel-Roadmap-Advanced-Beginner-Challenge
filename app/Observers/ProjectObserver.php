5e384a8ae1dc2b0f5d6dc446f4a6ee09e41fafb5
sodaler <misha_lector87@mail.ru>
2023-07-03 21:16:39 +0400

    Refactoring Project Observer

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -13,10 +13,10 @@ class ProjectObserver
     public function created(Project $project): void
     {
         if (auth()->user() !== null) {
-            Log::info('User: ' . auth()->user()->email . ' created new Project: ' . $project->id . '. ' . $project->title_company);
+            Log::info('User: ' . auth()->user()->email . ' created new Project: ' . $project->id . '. ' . $project->title);
         }
 
-        flash()->info('Project created: ' . $project->title_company);
+        flash()->info('Project created: ' . $project->title);
     }
 
     /**
@@ -25,10 +25,10 @@ class ProjectObserver
     public function updated(Project $project): void
     {
         if (auth()->user() !== null) {
-            Log::info('User: ' . auth()->user()->email . ' updated Project: ' . $project->id . '. ' . $project->title_company);
+            Log::info('User: ' . auth()->user()->email . ' updated Project: ' . $project->id . '. ' . $project->title);
         }
 
-        flash()->info('Project updated: ' . $project->title_company);
+        flash()->info('Project updated: ' . $project->title);
     }
 
     /**
@@ -37,10 +37,10 @@ class ProjectObserver
     public function deleted(Project $project): void
     {
         if (auth()->user() !== null) {
-            Log::info('User: ' . auth()->user()->email . ' deleted Project: ' . $project->id . '. ' . $project->title_company);
+            Log::info('User: ' . auth()->user()->email . ' deleted Project: ' . $project->id . '. ' . $project->title);
         }
 
-        flash()->info('Project deleted: ' . $project->title_company);
+        flash()->info('Project deleted: ' . $project->title);
     }
 
 }
