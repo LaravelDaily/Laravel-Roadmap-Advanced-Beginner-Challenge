5127c6188a402addf964caa21fe174e4b91a75b2
sodaler <misha_lector87@mail.ru>
2023-07-04 19:10:17 +0400

    Dashboard data count

--- app/Http/Controllers/Crm/HomeController.php
+++ app/Http/Controllers/Crm/HomeController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Crm;
 
 use App\Http\Controllers\Controller;
+use App\Models\Client;
+use App\Models\Project;
+use App\Models\Task;
 use function view;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('crm.main.index');
+        $data = [];
+        $data['clientsCount'] = Client::query()->count();
+        $data['projectsCount'] = Project::query()->count();
+        $data['tasksCount'] = Task::query()->count();
+
+        return view('crm.main.index', compact('data'));
     }
 }
