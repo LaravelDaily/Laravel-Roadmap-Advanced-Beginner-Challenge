0064ea563c4cacf26f7d635ffa4a3d20ee0e8033
nasirhumbatli <nesir.humbetli@gmail.com>
2024-05-08 16:45:21 +0400

    Route Redirect - homepage should automatically redirect to the login form

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::redirect('/', '/login');
 
 Route::get('/dashboard', function () {
