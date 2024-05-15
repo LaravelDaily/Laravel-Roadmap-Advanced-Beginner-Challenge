4da6411c7e63d1e6f563c9a4fdfb1f29deadca67
nasirhumbatli <nesir.humbetli@gmail.com>
2024-05-15 12:38:17 +0400

    Delete operation bug fixed in user and client index blade

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -99,16 +99,39 @@
                                                     </li>
                                                 </ul>
                                                 <div class="py-1">
-                                                    <form action="{{route('clients.destroy', $client)}}" method="POST">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="block px-4 py-2 text-sm w-full text-left text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                                                            Delete
-                                                        </button>
-                                                    </form>
+                                                    <x-simple-button
+                                                        class="block px-4 py-2 text-sm w-full text-left text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                                        x-data=""
+                                                        x-on:click.prevent="$dispatch('open-modal', 'confirm-client-deletion{{$loop->index}}')"
+                                                    >{{ __('Delete') }}</x-simple-button>
                                                 </div>
                                             </div>
+                                            <x-modal name="confirm-client-deletion{{$loop->index}}" focusable>
+                                                <form action="{{route('clients.destroy', $client)}}" method="POST" class="p-6">
+                                                    @csrf
+                                                    @method('delete')
+
+                                                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                        {{ __('Are you sure you want to delete this client?') }}
+                                                    </h2>
+
+                                                    <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                                        {{ __('Once this client is deleted, all of its resources and data will be unreachable.') }}
+                                                    </p>
+
+                                                    <div class="mt-6 flex justify-end">
+                                                        <x-secondary-button x-on:click="$dispatch('close')">
+                                                            {{ __('Cancel') }}
+                                                        </x-secondary-button>
+
+                                                        <x-danger-button class="ms-3">
+                                                            {{ __('Delete Client') }}
+                                                        </x-danger-button>
+                                                    </div>
+                                                </form>
+                                            </x-modal>
                                         </div>
+
                                     </td>
                                 </tr>
                             @empty
@@ -120,6 +143,7 @@
                     <div class="mt-3">
                         {{$clients->links()}}
                     </div>
+
                 </div>
             </div>
         </div>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -92,14 +92,38 @@
                                                     </a>
                                                 </li>
                                             </ul>
-                                            <div class="py-1 bg-red">
+                                            <div class="py-1">
                                                 <x-simple-button
                                                     class="block px-4 py-2 text-sm w-full text-left text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                                                     x-data=""
-                                                    x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
+                                                    x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion{{$loop->index}}')"
                                                 >{{ __('Delete') }}</x-simple-button>
                                             </div>
                                         </div>
+                                        <x-modal name="confirm-user-deletion{{$loop->index}}" focusable>
+                                            <form action="{{route('users.destroy', $user)}}" method="POST" class="p-6">
+                                                @csrf
+                                                @method('delete')
+
+                                                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                    {{ __('Are you sure you want to delete this account?') }}
+                                                </h2>
+
+                                                <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                                    {{ __('Once this account is deleted, all of its resources and data will be unreachable.') }}
+                                                </p>
+
+                                                <div class="mt-6 flex justify-end">
+                                                    <x-secondary-button x-on:click="$dispatch('close')">
+                                                        {{ __('Cancel') }}
+                                                    </x-secondary-button>
+
+                                                    <x-danger-button class="ms-3">
+                                                        {{ __('Delete User') }}
+                                                    </x-danger-button>
+                                                </div>
+                                            </form>
+                                        </x-modal>
                                     </div>
                                 </td>
                             </tr>
@@ -112,30 +136,6 @@
                     <div class="mt-3">
                         {{$users->links()}}
                     </div>
-                    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-                        <form action="{{route('users.destroy', $user)}}" method="POST" class="p-6">
-                            @csrf
-                            @method('delete')
-
-                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                                {{ __('Are you sure you want to delete this account?') }}
-                            </h2>
-
-                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                {{ __('Once this account is deleted, all of its resources and data will be unreachable.') }}
-                            </p>
-
-                            <div class="mt-6 flex justify-end">
-                                <x-secondary-button x-on:click="$dispatch('close')">
-                                    {{ __('Cancel') }}
-                                </x-secondary-button>
-
-                                <x-danger-button class="ms-3">
-                                    {{ __('Delete User') }}
-                                </x-danger-button>
-                            </div>
-                        </form>
-                    </x-modal>
                 </div>
             </div>
         </div>
