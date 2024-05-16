b164408ce7a6cd4d5bd9aca89dc52bcde93bdc13
nasirhumbatli <nesir.humbetli@gmail.com>
2024-05-16 10:21:35 +0400

    select option true false fixed to implement old helper method

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -75,7 +75,7 @@
                             <select id="status" required name="status" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="">Choose a status</option>
                                 <option value="1" @selected(old('status') == 1)>{{__('Open')}}</option>
-                                <option value="0" @selected(old('status') == 0)>{{__('Close')}}</option>
+                                <option value="0" @selected(old('status', 'empty') == 0)>{{__('Close')}}</option>
                             </select>
                             @error('status')
                             <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">{{$message}}</span></p>
