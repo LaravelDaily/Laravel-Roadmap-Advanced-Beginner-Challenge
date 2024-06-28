3bca3c5e2642ce77a0a436c62c96973c9a600118
nasirhumbatli <nesir.humbetli@gmail.com>
2024-06-28 17:25:08 +0400

    Some modification in api.php

--- routes/api.php
+++ routes/api.php
@@ -19,19 +19,19 @@ Route::post('login', function (Request $request) {
         'email' => 'required|email|max:250',
         'password' => 'required|string|max:250',
     ]);
-
     if (\Illuminate\Support\Facades\Auth::attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
         $token = auth()->user()->createToken('ApiToken')->plainTextToken;
         return response()->json(['token' => $token], 200);
     }
-
     return response()->json(['message' => 'Please check your Email or Password and try again to login.'], 401);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class)->except('store');
-    Route::apiResource('clients', ClientController::class);
-    Route::apiResource('projects', ProjectController::class);
-    Route::apiResource('tasks', TaskController::class);
+    Route::apiResources([
+        'clients' => ClientController::class,
+        'projects' => ProjectController::class,
+        'tasks' => TaskController::class,
+    ]);
 });
 
