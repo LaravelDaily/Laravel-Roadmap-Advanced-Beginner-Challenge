49410d2f75e8aa1e7efea5bf8ebdda33200368df
nasirhumbatli <nesir.humbetli@gmail.com>
2024-07-02 15:29:38 +0400

    Overrided API error exception in bootstrap/app.php for simple version

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,14 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'The requested resource could not be found.',
+                    'error' => $e->getMessage(),
+                ], Response::HTTP_NOT_FOUND);
+            }
+            return false;
+        });
     })->create();
